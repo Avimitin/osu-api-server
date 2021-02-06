10eac064a0ec70de4fb3bfba8a143c87d666cac9
avimitin <avimitin@gmail.com>
2021-02-06 08:32:52 +0000

    added database connection function

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,7 +1,23 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"time"
 
-func Connect() (*sql.DB, error) {
-	return nil, nil
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("connect %s:%v", dsn, err)
+	}
+	// Set limit
+	if db != nil {
+		db.SetConnMaxLifetime(time.Minute * 3)
+		db.SetMaxOpenConns(10)
+		db.SetMaxIdleConns(10)
+	}
+	return db, nil
 }
