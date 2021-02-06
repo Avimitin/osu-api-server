84b6af4fe2bd106777fca2078ab1ba171c5e8ece
avimitin <avimitin@gmail.com>
2021-02-06 12:45:02 +0800

    added database struct

--- internal/config/type.go
+++ internal/config/type.go
@@ -1,5 +1,12 @@
 package config
 
 type Configuration struct {
-	Key string `json:"key"`
+	Key   string         `json:"key"`
+	DBSec DatabaseSecret `json:"dbs"`
+}
+
+type DatabaseSecret struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
 }
