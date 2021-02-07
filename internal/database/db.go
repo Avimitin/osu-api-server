5b226f5985ae8fa06c5a906a6a4ecf1f13b8c554
avimitin <avimitin@gmail.com>
2021-02-07 17:49:13 +0800

    added User struct and database operation
    - added GetUserRecent method
    - added InsertNewUser method
    - modified table init functions for accepting values

--- internal/database/db.go
+++ internal/database/db.go
@@ -31,22 +31,71 @@ func (db *OsuDB) InitTable() error {
 	return initUserTable(db.DB)
 }
 
+type User struct {
+	UserID    string `json:"user_id"`
+	Username  string `json:"username"`
+	PlayCount string `json:"play_count"`
+	Rank      string `json:"rank"`
+	PP        string `json:"pp"`
+	Acc       string `json:"acc"`
+}
+
+func (db *OsuDB) GetUserRecent(username string) (*User, error) {
+	const query = "SELECT username, playcount, rank, pp, acc FROM users WHERE username = ? OR user_id = ?"
+	var u *User
+	stmtOut, err := db.DB.Prepare(query)
+	err = stmtOut.QueryRow(username).Scan(&u)
+	if err != nil {
+		return nil, fmt.Errorf("query %s : %v", query, err)
+	}
+	if u == nil {
+		return nil, fmt.Errorf("user %s not found", username)
+	}
+	return u, nil
+}
+
+func (db *OsuDB) InsertNewUser(userID string, username string, pc string, rank string, pp string, acc string) (*User, error) {
+	const query = `INSERT INTO users (
+	user_id, username, playcount, rank, pp, acc
+) VALUES (
+	?,?,?,?,?,?
+)
+`
+	stmtIn, err := db.DB.Prepare(query)
+	if err != nil {
+		return nil, fmt.Errorf("query %s : %v", query, err)
+	}
+	_, err = stmtIn.Exec(userID, username, pc, rank, pp, acc)
+	if err != nil {
+		return nil, fmt.Errorf("insert %s: %v", query, err)
+	}
+	return &User{
+		UserID:    userID,
+		Username:  username,
+		PlayCount: pc,
+		Rank:      rank,
+		PP:        pp,
+		Acc:       acc,
+	}, nil
+}
+
 func initUserTable(db *sql.DB) error {
 	const userTable = `
 CREATE TABLE IF NOT EXIST users(
-id INT AUTO_INCREMENT,
-user_id BIGINT,
-username VARCHAR(255),
-playcount BIGINT,
-rank INT,
-pp INT,
-acc DOUBLE,
-playcount_ytd BIGINT,
-rank_ytd INT,
-pp_ytd INT,
-acc_ytd DOUBLE,
-total_play_ytd BIGINT,
-PRIMARY KEY (id)
+	id INT AUTO_INCREMENT,
+	user_id VARCHAR(18) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	playcount VARCHAR(18),
+	rank VARCHAR(18),
+	pp VARCHAR(18),
+	acc VARCHAR(18),
+	total_play VARCHAR(18),
+	playcount_ytd VARCHAR(18),
+	rank_ytd VARCHAR(18),
+	pp_ytd VARCHAR(18),
+	acc_ytd VARCHAR(18),
+	total_play_ytd VARCHAR(18),
+	PRIMARY KEY (id)
 )CHARSET=utf8mb4
 	`
 	_, err := db.Exec(userTable)
