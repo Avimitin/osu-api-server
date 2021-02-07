ce2b0568e9488f8041da9f7b64ed6da73b750adf
avimitin <avimitin@gmail.com>
2021-02-07 20:54:39 +0800

    added ytd field in user struct

--- internal/database/db.go
+++ internal/database/db.go
@@ -33,12 +33,18 @@ func (db *OsuDB) InitTable() error {
 }
 
 type User struct {
-	UserID    string `json:"user_id"`
-	Username  string `json:"username"`
-	PlayCount string `json:"play_count"`
-	Rank      string `json:"rank"`
-	PP        string `json:"pp"`
-	Acc       string `json:"acc"`
+	UserID       string `json:"user_id"`
+	Username     string `json:"username"`
+	PlayCount    string `json:"play_count"`
+	Rank         string `json:"rank"`
+	PP           string `json:"pp"`
+	Acc          string `json:"acc"`
+	TotalPlay    string `json:"total_play"`
+	PcYtd        string `json:"play_count_ytd"`
+	RankYtd      string `json:"rank_ytd"`
+	PpYtd        string `json:"pp_ytd"`
+	AccYtd       string `json:"acc_ytd"`
+	TotalPlayYtd string `json:"ttp_ytd"`
 }
 
 func (db *OsuDB) GetUserRecent(username string) (*User, error) {
