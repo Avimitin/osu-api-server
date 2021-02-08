80d512e2f0d0ec6b8a034db216cc2d993bcafc08
avimitin <avimitin@gmail.com>
2021-02-08 18:01:25 +0800

    added basic http request handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,10 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func OsuServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `{"username": "avimitin"}`)
+}
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,1 +1,23 @@
+package server
 
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPlayer(t *testing.T) {
+	t.Run("Get avimitin score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/api/v1/players/avimitin", nil)
+		response := httptest.NewRecorder()
+
+		OsuServer(response, request)
+
+		got := response.Body.String()
+		want := `{"username": "avimitin"}`
+
+		if got != want {
+			t.Errorf("want %s got %s", want, got)
+		}
+	})
+}
