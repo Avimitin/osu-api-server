aa63c320f81c913efaede2367374e96a26a9d7e5
avimitin <avimitin@gmail.com>
2021-02-10 11:19:03 +0800

    added logging for listen port

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,6 +13,7 @@ func main() {
 		Data: &server.OsuPlayerData{},
 	}
 
+	log.Println("server listening on port 11451")
 	if err := http.ListenAndServe(":11451", OsuSer); err != nil {
 		log.Fatalf("handle %s : %v", ":11451", err)
 	}
