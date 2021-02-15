6934475e33fec641760d4e9cf1ccf17dd7d1cc01
avimitin <avimitin@gmail.com>
2021-02-15 20:23:51 +0800

    added MySQLDataStore as db; added db initialize function

--- internal/database/mysql_test.go
+++ internal/database/mysql_test.go
@@ -1,24 +1,38 @@
 package database
 
 import (
-	"database/sql"
+	"fmt"
+	"testing"
 
 	"github.com/go-testfixtures/testfixtures/v3"
 )
 
 var (
-	db       *sql.DB
+	db       *MySQLDataStore
 	fixtures *testfixtures.Loader
 )
 
 func InitTestfixtures(dir string) error {
 	var err error
+	db, err = NewMySQLStore(
+		"osu_test:osu_test@tcp(127.0.0.1:3306)/osu?charset=utf8mb4",
+	)
+	if err != nil {
+		return fmt.Errorf("connect to database: %v", err)
+	}
 	testFiles := testfixtures.Files(dir)
 	fixtures, err = testfixtures.New(
-		testfixtures.Database(db),
+		testfixtures.Database(db.db),
 		testfixtures.Dialect("mysql"),
 		testfixtures.DangerousSkipTestDatabaseCheck(),
 		testFiles,
 	)
 	return err
+}
+
+func prepareTestDatabase(t testing.TB) {
+	t.Helper()
+	if err := fixtures.Load(); err != nil {
+		t.Fatalf("loading fixtures: %v", err)
+	}
 }
