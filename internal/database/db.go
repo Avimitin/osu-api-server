a928242502f3776f17650257a9818fb2f902e0d3
avimitin <avimitin@gmail.com>
2021-02-16 15:37:16 +0800

    fixed out of dated user return

--- internal/database/db.go
+++ internal/database/db.go
@@ -51,46 +51,58 @@ func (db *OsuDB) GetUserYtd(username string) (*User, error) {
 
 // InsertNewUser insert user data into database
 func (db *OsuDB) InsertNewUser(
-	userID string, username string, pc string, rank string, pp string, acc string, total_play string,
+	userID string, username string, pc string, rank string, pp string, acc string, playtime string,
 ) error {
 	return db.UsersData.AddPlayer(
 		User{
-			UserID:    userID,
-			Username:  username,
-			PlayCount: Date{Recent: pc},
-			Rank:      Date{Recent: rank},
-			PP:        Date{Recent: pp},
-			Acc:       Date{Recent: acc},
-			PlayTime:  Date{Recent: total_play},
+			UserID:   userID,
+			Username: username,
+			Date: Date{
+				Recent: Data{
+					PlayCount: pc,
+					Rank:      rank,
+					PP:        pp,
+					Acc:       acc,
+					PlayTime:  playtime,
+				},
+			},
 		},
 	)
 }
 
-// UpdateUser update user data with given data
+// UpdateUser update user recent data with given data
 func (db *OsuDB) UpdateUser(
-	username string, pc string, rank string, pp string, acc string, total_play string,
+	username string, pc string, rank string, pp string, acc string, playtime string,
 ) error {
 	return db.UsersData.Update(
 		User{
-			Username:  username,
-			PlayCount: Date{Recent: pc},
-			Rank:      Date{Recent: rank},
-			PP:        Date{Recent: pp},
-			Acc:       Date{Recent: acc},
-			PlayTime:  Date{Recent: total_play},
+			Username: username,
+			Date: Date{
+				Recent: Data{
+					PlayCount: pc,
+					Rank:      rank,
+					PP:        pp,
+					Acc:       acc,
+					PlayTime:  playtime,
+				},
+			},
 		})
 }
 
 func (db *OsuDB) UpdateUserYtd(
-	username string, pc string, rank string, pp string, acc string, total_play string,
+	username string, pc string, rank string, pp string, acc string, playtime string,
 ) error {
 	return db.UsersData.UpdateOld(
 		User{
-			Username:  username,
-			PlayCount: Date{Yesterday: pc},
-			Rank:      Date{Yesterday: rank},
-			PP:        Date{Yesterday: pp},
-			Acc:       Date{Yesterday: acc},
-			PlayTime:  Date{Yesterday: total_play},
+			Username: username,
+			Date: Date{
+				Yesterday: Data{
+					PlayCount: pc,
+					Rank:      rank,
+					PP:        pp,
+					Acc:       acc,
+					PlayTime:  playtime,
+				},
+			},
 		})
 }
