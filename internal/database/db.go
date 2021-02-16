ebeac524dbac373dfd17b82ec0e9ee7ebf47598c
avimitin <avimitin@gmail.com>
2021-02-16 10:14:56 +0800

    moved User struct to type.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -39,22 +39,6 @@ func (db *OsuDB) CheckUserDataStoreHealth() error {
 	return db.UsersData.CheckHealth()
 }
 
-// User type contain user field
-type User struct {
-	UserID       string `json:"user_id"`
-	Username     string `json:"username"`
-	PlayCount    string `json:"play_count"`
-	Rank         string `json:"rank"`
-	PP           string `json:"pp"`
-	Acc          string `json:"acc"`
-	TotalPlay    string `json:"total_play"`
-	PcYtd        string `json:"play_count_ytd"`
-	RankYtd      string `json:"rank_ytd"`
-	PpYtd        string `json:"pp_ytd"`
-	AccYtd       string `json:"acc_ytd"`
-	TotalPlayYtd string `json:"ttp_ytd"`
-}
-
 // GetUserRecent return user data with given name
 func (db *OsuDB) GetUserRecent(username string) (*User, error) {
 	return db.UsersData.GetPlayer(username)
