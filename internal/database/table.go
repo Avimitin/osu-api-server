f94399f37aacb7d20d74d38952e4446df096c7e8
avimitin <avimitin@gmail.com>
2021-02-16 11:55:56 +0800

    added replace function at variable declare

--- internal/database/table.go
+++ internal/database/table.go
@@ -2,18 +2,18 @@ package database
 
 import "strings"
 
-const (
-	tableUsers = `
+var (
+	tableUsers = replace(`
 CREATE TABLE IF NOT EXISTS users (
 	"id"       INT NOT NULL AUTO_INCREMENT,
 	"user_id"  TINYTEXT NOT NULL UNIQUE,
 	"username" TINYTEXT NOT NULL,
 	PRIMARY  KEY (id)
 )CHARSET=utf8mb4
-`
+`)
 
-	tableRecentData = `
- CREATE TABLE IF NOT EXISTS recent_data ( 
+	tableRecentData = replace(`
+CREATE TABLE IF NOT EXISTS recent_data ( 
 	 "id"         INT NOT NULL,
 	 "play_count" TINYTEXT NOT NULL,
 	 "rank"       TINYTEXT NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE IF NOT EXISTS users (
 	 "play_time"  TINYTEXT NOT NULL,
 	 FOREIGN    KEY(id) REFERENCES users(id)
  )CHARSET=utf8mb4
-`
+`)
 
-	tableYesterdayData = `
+	tableYesterdayData = replace(`
  CREATE TABLE IF NOT EXISTS yesterday_data ( 
 	 "id"         INT NOT NULL,
 	 "play_count" TINYTEXT NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS users (
 	 "play_time"  TINYTEXT NOT NULL,
 	 FOREIGN    KEY(id) REFERENCES users(id)
  )CHARSET=utf8mb4
-`
+`)
 )
 
 // replace replace all the double quote to backtick
