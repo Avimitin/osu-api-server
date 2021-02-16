7e0bc9abdd3284e48b25bc13d5b71a7284a4580e
avimitin <avimitin@gmail.com>
2021-02-16 10:23:14 +0800

    added TestMain function

--- internal/database/mysql_test.go
+++ internal/database/mysql_test.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/go-testfixtures/testfixtures/v3"
@@ -35,4 +36,20 @@ func prepareTestDatabase(t testing.TB) {
 	if err := fixtures.Load(); err != nil {
 		t.Fatalf("loading fixtures: %v", err)
 	}
+}
+
+func TestMain(m *testing.M) {
+	var err error
+
+	err = InitTestfixtures(os.Getenv("osuapi_project_root"))
+	if err != nil {
+		fatalF("init test fixtures: %v", err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func fatalF(context string, args ...interface{}) {
+	fmt.Printf(context, args...)
+	os.Exit(1)
 }
