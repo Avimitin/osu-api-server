cf089f674b2b14b7b67c0c38d4258244e43c3cdc
avimitin <avimitin@gmail.com>
2021-02-19 14:28:45 +0800

    separated PlayerData code and server code

---
+++ internal/server/dataStore.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/avimitin/osu-api-server/internal/api"
+	"github.com/avimitin/osu-api-server/internal/database"
+)
+
+type OsuPlayerData struct {
+	db *database.OsuDB
+}
+
+// NewOsuPlayerData return a database controller which satisfy PlayerData interface
+func NewOsuPlayerData(db *database.OsuDB) *OsuPlayerData {
+	return &OsuPlayerData{db}
+}
+
+func (opd *OsuPlayerData) GetPlayerStat(name string) (*Player, error) {
+	return getPlayerDataByName(name, opd.db)
+}
+
+func getPlayerDataByName(name string, db *database.OsuDB) (*Player, error) {
+	u, e := api.GetUsers(name)
+	if e != nil {
+		return nil, e
+	}
+	if len(u) <= 0 {
+		return nil, errors.New("user " + name + " not found")
+	}
+	user := u[0]
+	lu, e := db.GetUserRecent(user.Username)
+	if e != nil {
+		if strings.Contains(e.Error(), "user") {
+			e = db.InsertNewUser(
+				user.UserID, user.Username, user.Playcount, user.PpRank,
+				user.PpRaw, user.Accuracy, user.TotalSecondsPlayed,
+			)
+			if e != nil {
+				return nil, fmt.Errorf("insert user %s : %v", name, e)
+			}
+			log.Printf("inserted %s into database", user.Username)
+		} else {
+			return nil, fmt.Errorf("query user %s: %v", name, e)
+		}
+	}
+	var diff *Different
+	if lu != nil {
+		diff, e = getUserDiff(user, "recent", &lu.Date)
+	}
+	if e != nil {
+		return nil, e
+	}
+
+	p := &Player{
+		Data: u[0],
+		Diff: diff,
+	}
+	return p, nil
+}
