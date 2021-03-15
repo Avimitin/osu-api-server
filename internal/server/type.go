838af027c445c25317e64bd78fbb3bfb06366ee8
Avimitin <avimitin@gmail.com>
2021-03-15 22:11:40 +0800

    added descriptions for type

--- internal/server/type.go
+++ internal/server/type.go
@@ -2,17 +2,21 @@ package server
 
 import "github.com/avimitin/osu-api-server/internal/api"
 
+// OsuData handle osu data
 type OsuData interface {
 	GetPlayerStat(name string) (*Player, error)
 	GetRecent(name, mapID string, perfect bool) (*api.RecentPlay, error)
 	GetBeatMaps(setID, mapID string) (*api.Beatmap, error)
 }
 
+// Player store user information and different between
+// each request
 type Player struct {
 	Data *api.User  `json:"latest_data"`
 	Diff *Different `json:"diff"`
 }
 
+// Different specific user data different
 type Different struct {
 	PlayCount string `json:"play_count"`
 	Rank      string `json:"rank"`
